6f1b818a499cdfdae07a5685b11148dc63ef8c9b
Wouter van Dam <wouter@journeyworks.nl>
2025-02-21 14:54:36 +0100

    fix build errors

--- src/services/fundermaps/index.ts
+++ src/services/fundermaps/index.ts
@@ -1,11 +1,9 @@
 import auth from './endpoints/auth'
 import building from './endpoints/building'
-import incident from './endpoints/incident'
 import user from './endpoints/user'
 
 export default {
   auth,
   building,
-  incident,
   user,
 }
